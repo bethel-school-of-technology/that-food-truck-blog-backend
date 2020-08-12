a5175a706f21e39e465d8d3b9b6b8c823a2cad76
Mitch McCuen <mitchmccuen@gmail.com>
2020-08-12 14:22:14 -0400

    changed methods to *

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
-    'Access-Control-Allow-Methods: POST, PUT, GET, OPTIONS, DELETE',
+    'Access-Control-Allow-Methods: *',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   next();
