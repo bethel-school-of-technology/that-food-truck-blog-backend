becee901c377d96728d47c674a950ecfff733aa9
Mitch McCuen <mitchmccuen@gmail.com>
2020-08-12 23:39:35 -0400

    added Access-Control-Allow-Methods: GET, PUT, POST, DELETE, HEAD to serverjs

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    'Access-Control-Allow-Methods: GET, PUT, POST, DELETE, HEAD'
   );
   next();
 });
